b2359592b40da0325df301b77e4ee8c2fbe8c5d7
Tanmay Thakur <65679447+tanmay1024@users.noreply.github.com>
2025-01-25 00:43:01 +0530

    Updated paligemma processor to include the class

--- processing_paligemma.py
+++ processing_paligemma.py
@@ -6,3 +6,8 @@ import torch
 IMAGENET_STANDARD_MEAN = [0.5, 0.5, 0.5]
 IMAGENET_STANDARD_STD = [0.5, 0.5, 0.5]
 
+class PaliGemmaProcessor:
+    def __init__(self, tokenizer, num_image_tokens: int, image_size: int):
+        super().init()
+        self.image_seq_length = num_image_tokens
+        self.image_size = image_size
